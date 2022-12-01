e85596b47d6a2637849b2af3837b0d7f0c778c24
olehae <das.typ.2003@gmail.com>
2022-12-02 00:15:47 +0100

    add to uebung02

--- uebung02/EuklidKlassisch.java
+++ uebung02/EuklidKlassisch.java
@@ -11,14 +11,16 @@ public class EuklidKlassisch {
         // ggT(x,y)                                    // K1
         System.out.println("| K1 \t | "+x+"\t | "+y+" \t | ggT("+x+", "+y+")");
         while ( y != 0 ) {
-                System.out.println("| K2 \t | "+x+"\t | "+y+" \t |   while("+y+" != 0)");
-                System.out.println("| K3 \t | "+x+"\t | "+y+" \t |     if("+x+" > "+y+")");             >                if ( x > y ){                               // K3
-                        int xbef = x;
-                        x = x - y;                             // K4
-                        System.out.println("| K4 \t | "+x+"\t | "+y+" \t |       x = "+xbef+" - "+y);   >                else{
-                        int ybef = y;
-                        y = y - x;                             // K5
-                        System.out.println("| K5 \t | "+x+"\t | "+y+" \t |       y = "+ybef+" - "+x);}
+            System.out.println("| K2 \t | "+x+"\t | "+y+" \t |   while("+y+" != 0)");
+            System.out.println("| K3 \t | "+x+"\t | "+y+" \t |     if("+x+" > "+y+")");                                  // K2
+            if ( x > y ){                               // K3
+                int xbef = x;
+                x = x - y;                             // K4
+                System.out.println("| K4 \t | "+x+"\t | "+y+" \t |       x = "+xbef+" - "+y);                   }
+            else{
+                int ybef = y;
+                y = y - x;                             // K5
+                System.out.println("| K5 \t | "+x+"\t | "+y+" \t |       y = "+ybef+" - "+x);}
         }
         System.out.println("| K2 \t | "+x+"\t | "+y+" \t |   while("+y+" != 0)");
         System.out.println("| K6 \t | "+x+"\t | "+y+" \t | = "+x);                         // K6
